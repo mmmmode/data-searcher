fb41a004309866abf9b963f883dd175eeee6edee
mode <870497980@qq.com>
2018-07-26 14:19:51 +0800

    new branch
    new branch

--- app/src/main/java/com/uestc/mode/modetest/MainActivity.java
+++ app/src/main/java/com/uestc/mode/modetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.uestc.mode.modetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
     }
 }
